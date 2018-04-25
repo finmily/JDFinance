e382f10f69338e7bdf09f89af056c454a9e05993
finmily.fan <finmily.fan@yeamoney.cn>
2018-04-25 19:20:34 +0800

    5-3 before

---
+++ app/js/core/panel.vue
@@ -0,0 +1,33 @@
+<template>
+  <section :class="[pannelClass,cname]">
+    <h4>-{{ title }}-</h4>
+    <slot/>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pannelClass: "pannel"
+    };
+  },
+  props: {
+    cname: {
+      type: String,
+      default: ""
+    },
+    title: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  @import "../../css/element.scss";
+  .pannel {
+    @include pannel;
+  }
+</style>
---
+++ app/js/public/header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <span class="left">
+      <em>注册</em>&nbsp;|<em>登录</em>
+    </span>
+    <btn>APP下载</btn>
+  </div>
+</template>
+
+<script>
+import btn from "../core/btn.vue";
+export default {
+  components: {
+    btn,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+    .header {
+      
+    }
+</style>
